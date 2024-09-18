8148876931c6e7540d7049e2d0d812b6bc342bfd
tbunny-n <defaultsnuzzers@gmail.com>
2024-09-17 23:49:04 -0600

    Daylight events

--- src/main/kotlin/snuz/vamp/Vamp.kt
+++ src/main/kotlin/snuz/vamp/Vamp.kt
@@ -19,7 +19,7 @@ object Vamp : ModInitializer {
     private val RaijinPositions: HashMap<ServerPlayerEntity, Vec3d> = HashMap()
 
     private const val SANGUINARE_INCREMENT_AMOUNT: Float = 0.2f
-    private const val DAYLIGHT_TICK_INTERVAL: Long = 5L
+    private const val TEN_MIN_DURATION: Int = 11000
 
     override fun onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -32,42 +32,66 @@ object Vamp : ModInitializer {
             server.worlds.forEach { world ->
                 val worldTime = world.timeOfDay % 24000
                 if (worldTime in 14000..23000 && lastWorldTime < 14000) {
-                    logger.info("Happy halloween fucker")
                     // Apply nighttime status effects
                     server.playerManager.playerList.forEach { player ->
                         val playerState = StateSaverAndLoader.getPlayerState(player)
                         if (playerState != null) {
                             if (playerState.isVampire || playerState.hasSanguinare) {
+                                playerState.sanguinareProgress += SANGUINARE_INCREMENT_AMOUNT // Progress sanguinare
+
                                 player.removeStatusEffect(
                                     StatusEffects.WEAKNESS
                                 )
                                 player.addStatusEffect(
                                     StatusEffectInstance(
                                         StatusEffects.NIGHT_VISION,
-                                        11000,
+                                        TEN_MIN_DURATION,
                                         0,
                                     )
                                 )
+
+                                val strengthAmp = when {
+                                    playerState.vampireLevel > 33 -> 2
+                                    playerState.sanguinareProgress > 1.4 -> 1
+                                    else -> 0
+                                }
                                 player.addStatusEffect(
                                     StatusEffectInstance(
                                         StatusEffects.STRENGTH,
-                                        11000,
-                                        0,
+                                        TEN_MIN_DURATION,
+                                        strengthAmp,
                                     )
                                 )
+
+                                val speedAmp = when {
+                                    playerState.vampireLevel > 38 -> 2
+                                    playerState.sanguinareProgress > 1.2 -> 1
+                                    else -> 0
+                                }
+                                player.addStatusEffect(
+                                    StatusEffectInstance(
+                                        StatusEffects.SPEED,
+                                        TEN_MIN_DURATION,
+                                        speedAmp,
+                                    )
+                                )
+
+                                val resistanceAmp = when {
+                                    playerState.vampireLevel > 41 -> 2
+                                    playerState.vampireLevel > 18 -> 1
+                                    else -> 0
+                                }
                                 player.addStatusEffect(
                                     StatusEffectInstance(
                                         StatusEffects.RESISTANCE,
-                                        11000,
-                                        0,
+                                        TEN_MIN_DURATION,
+                                        resistanceAmp,
                                     )
                                 )
                             }
                         }
                     }
-                } else if (worldTime in 1000..12000 && lastWorldTime > 12000) {
-                    // Apply daytime status effects
-                    logger.info("WEEHEEHEEHEE")
+                } else if (worldTime in 200..12000 && lastWorldTime > 12000) {
                     // Apply daytime status effects
                     server.playerManager.playerList.forEach { player ->
                         val playerState = StateSaverAndLoader.getPlayerState(player)
@@ -82,10 +106,13 @@ object Vamp : ModInitializer {
                                 player.removeStatusEffect(
                                     StatusEffects.RESISTANCE
                                 )
+                                player.removeStatusEffect(
+                                    StatusEffects.SPEED
+                                )
                                 player.addStatusEffect(
                                     StatusEffectInstance(
                                         StatusEffects.WEAKNESS,
-                                        11000,
+                                        TEN_MIN_DURATION,
                                         0,
                                     )
                                 )
