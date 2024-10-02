1aa903294b33aba983d255dd83f807f592c0654c
tbunny-n <defaultsnuzzers@gmail.com>
2024-10-01 22:29:59 -0600

    Client-side cooldowns

--- src/client/kotlin/snuz/vamp/VampClient.kt
+++ src/client/kotlin/snuz/vamp/VampClient.kt
@@ -23,10 +23,14 @@ import snuz.vamp.network.CloakAbilityPayload
 import snuz.vamp.network.FlyingRaijinPayload
 
 object VampClient : ClientModInitializer {
-    private const val RAIJIN_COOLDOWN: Double = 1.5
-    private const val CLOAK_COOLDOWN: Double = 1.5
-    private const val SPEED_COOLDOWN: Double = 1.5
-    private const val AMATERASU_COOLDOWN: Double = 1.5
+    private const val RAIJIN_COOLDOWN: Int = 30
+    private const val CLOAK_COOLDOWN: Int = 50
+    private const val SPEED_COOLDOWN: Int = 50
+    private const val AMATERASU_COOLDOWN: Int = 500
+    private var lastRaijin = RAIJIN_COOLDOWN
+    private var lastCloak = CLOAK_COOLDOWN
+    private var lastSpeed = SPEED_COOLDOWN
+    private var lastAmaterasu = AMATERASU_COOLDOWN
 
     private lateinit var RAIJIN_KEYBIND: KeyBinding
     private lateinit var CLOAK_KEYBIND: KeyBinding
@@ -47,34 +51,22 @@ object VampClient : ClientModInitializer {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
         RAIJIN_KEYBIND = KeyBindingHelper.registerKeyBinding(
             KeyBinding(
-                "key.vamp.raijin",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_C,
-                "category.vamp"
+                "key.vamp.raijin", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "category.vamp"
             )
         )
         CLOAK_KEYBIND = KeyBindingHelper.registerKeyBinding(
             KeyBinding(
-                "key.vamp.cloak",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_V,
-                "category.vamp"
+                "key.vamp.cloak", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "category.vamp"
             )
         )
         SPEED_KEYBIND = KeyBindingHelper.registerKeyBinding(
             KeyBinding(
-                "key.vamp.speed",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_R,
-                "category.vamp"
+                "key.vamp.speed", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "category.vamp"
             )
         )
         AMATERASU_KEYBIND = KeyBindingHelper.registerKeyBinding(
             KeyBinding(
-                "key.vamp.amaterasu",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_X,
-                "category.vamp"
+                "key.vamp.amaterasu", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_X, "category.vamp"
             )
         )
 
@@ -90,19 +82,33 @@ object VampClient : ClientModInitializer {
         ClientTickEvents.END_CLIENT_TICK.register { client ->
             while (RAIJIN_KEYBIND.wasPressed()) {
                 // Tell the server that client wants to use flying raijin
+                if (lastRaijin < RAIJIN_COOLDOWN) break
                 ClientPlayNetworking.send(FlyingRaijinPayload)
+                lastRaijin = 0
             }
             while (CLOAK_KEYBIND.wasPressed()) {
+                if (lastCloak < CLOAK_COOLDOWN) break
                 ClientPlayNetworking.send(CloakAbilityPayload)
+                lastCloak = 0
             }
             while (AMATERASU_KEYBIND.wasPressed()) {
+                if (lastAmaterasu < AMATERASU_COOLDOWN) break
                 // Get blocks player is looking at
                 val amaterasuPayload = amaterasuClient(client) ?: break // am i stupid
                 ClientPlayNetworking.send(amaterasuPayload)
+                lastAmaterasu = 0
             }
             while (SPEED_KEYBIND.wasPressed()) {
-                TODO()
+                if (lastSpeed < SPEED_COOLDOWN) break
+                // TODO:
+                lastSpeed = 0
             }
+
+            // Increment tick counters for cooldowns
+            lastRaijin++
+            lastCloak++
+            lastAmaterasu++
+            lastSpeed++
         }
     }
 
